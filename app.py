d53f505ce957285216b10536fbe5d40ab3f92a57
JackDuncan15 <Duncan-J15@ulster.ac.uk>
2026-02-19 19:13:36 +0000

    added table code to index.html and added database connection and table variables to app.py

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
+import sqlite3
 from flask import Flask, render_template
 app = Flask(__name__)
 
+headings = ("Company","Title","Status")
+
+def get_db_connection():
+    conn = sqlite3.connect('JobTrackDB.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 @app.route('/')
 def home():
-   return render_template('index.html')
+   conn = get_db_connection()
+   columns = [row[1] for row in conn.execute("PRAGMA table_info(JobApps)")]
+   selected_columns = [column for column in columns if column != 'id']
+   query = f"SELECT {', '.join(selected_columns)} FROM JobApps"
+   data = conn.execute(query)
+   return render_template('index.html', headings=headings, data=data)
 if __name__ == '__main__':
    app.run()
