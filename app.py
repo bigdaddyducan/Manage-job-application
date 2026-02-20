d8f829026d2970483cc3d3b864c7b435ee6f1bd0
JackDuncan15 <Duncan-J15@ulster.ac.uk>
2026-02-20 15:09:26 +0000

    added css code along with adding relevant python and html code to include numbers representing the count of each variable

--- app.py
+++ app.py
@@ -9,6 +9,24 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
+def get_applied_count():
+   conn = get_db_connection()
+   applied = conn.execute("SELECT COUNT(*) from JobApps WHERE status = 'Applied'").fetchone()[0]
+   conn.close()
+   return applied
+
+def get_rejected_count():
+   conn = get_db_connection()
+   rejected = conn.execute("SELECT COUNT(*) from JobApps WHERE status = 'Rejected'").fetchone()[0]
+   conn.close()
+   return rejected
+
+def get_interviewed_count():
+   conn = get_db_connection()
+   interviewed = conn.execute("SELECT COUNT(*) from JobApps WHERE status = 'Interviewed'").fetchone()[0]
+   conn.close()
+   return interviewed
+
 @app.route('/')
 def home():
    conn = get_db_connection()
@@ -16,6 +34,10 @@ def home():
    selected_columns = [column for column in columns if column != 'id']
    query = f"SELECT {', '.join(selected_columns)} FROM JobApps"
    data = conn.execute(query)
-   return render_template('index.html', headings=headings, data=data)
+   applied = get_applied_count()
+   rejected = get_rejected_count()
+   interviewed = get_interviewed_count()
+
+   return render_template('index.html', headings=headings, data=data,applied=applied,rejected=rejected,interviewed=interviewed)
 if __name__ == '__main__':
    app.run()
